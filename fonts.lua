d254cd8bf04d6a960599d2983de0e7a921a124a8
ebina4yaka <ebina4yaka@protonmail.com>
2023-01-26 07:05:32 +0900

    update config

--- fonts.lua
+++ fonts.lua
@@ -2,11 +2,31 @@ local wezterm = require('wezterm')
 
 local font = wezterm.font
 
+local DPI_CHANGE_NUM = 90
+local DPI_CHANGE_FONT_SIZE = 14
+
+local prev_dpi = 0
+
+wezterm.on('window-focus-changed', function(window, pane)
+  local dpi = window:get_dimensions().dpi
+
+  if dpi == prev_dpi then
+    return
+  end
+
+  local overrides = window:get_config_overrides() or {}
+  overrides.font_size = dpi >= DPI_CHANGE_NUM and DPI_CHANGE_FONT_SIZE or nil
+
+  window:set_config_overrides(overrides)
+
+  prev_dpi = dpi
+end)
+
 return {
   font = font('CaskaydiaCove Nerd Font', {
     weight = 'Regular',
     stretch = 'Normal',
     style = 'Normal',
   }),
-  font_size = 15,
+  font_size = 16,
 }
--- wezterm.lua
+++ wezterm.lua
@@ -1,6 +1,9 @@
+local wezterm = require('wezterm')
+
 local utils = require('utils')
 local keys = require('keys')
 local fonts = require('fonts')
+require('status')
 
 local default_prog = { 'zsh', '-l' }
 local launch_menu = {}
@@ -27,15 +30,16 @@ local config = {
   adjust_window_size_when_changing_font_size = true,
   launch_menu = launch_menu,
   keys = keys,
+  status_update_interval = 1000,
 }
 
 local appearance_config = {
-  hide_tab_bar_if_only_one_tab = true,
   font = fonts.font,
   font_size = fonts.font_size,
   text_background_opacity = 0.9,
   window_background_opacity = 0.85,
   color_scheme = 'Overnight Slumber',
+  tab_bar_at_bottom = true,
 }
 
 return utils.merge_tables(config, appearance_config)
